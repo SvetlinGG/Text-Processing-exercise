30ddeb03052165d410e3acca61d5a60af6ad09a9
Svetlin Garabedyan <146958421+SvetlinGG@users.noreply.github.com>
2024-05-01 20:20:57 +0200

    Add files via upload

---
+++ Substring2.js
@@ -0,0 +1,8 @@
+function substring(word, start,count){
+
+    let text = word.substring(start,start + count);
+    console.log(text);
+
+}
+substring('ASentence', 1, 8);
+substring('SkipWord', 4, 7)
